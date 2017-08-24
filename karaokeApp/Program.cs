3db8a7f7a79c71562d151d58b0410199a819b6b3
George Codat Tudoran <office@solcreation.ro>
2017-08-24 04:07:34 +0300

    Choosing the file to be played

--- karaokeApp/Program.cs
+++ karaokeApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace KaraokeApp
 {
@@ -10,7 +11,44 @@ namespace KaraokeApp
     {
         static void Main(string[] args)
         {
+            var songs = new string[] { "allthistime", "endoftheworld", "itsasmallworld" };
             Console.WriteLine("Welcome to Singer");
+            while (true)
+            {
+                Console.WriteLine("Please choose a song you would like to sing");
+                Console.WriteLine("1 for Jonathan Coulton's All This Time");
+                Console.WriteLine("2 for End of the World");
+                Console.WriteLine("3 for It's A Small World");
+                Console.WriteLine("Type 'Quit' to quit the application");
+
+                var response = Console.ReadLine();
+
+                if (response.ToLower() == "quit")
+                    break;
+
+                var song = 0;
+                if(!int.TryParse(response, out song))
+                    {
+                        Console.WriteLine("Invalid Input, Try again.");
+                        continue;
+                    }
+                    song--;
+                    var fileName = songs[song] + ".txt";
+                    Console.WriteLine(fileName);
+
+                if(!File.Exists(fileName) {
+                    Console.WriteLine("File not found, Try again");
+                    continue;
+                }
+
+                var lines = File.ReadAllLines(fileName);
+                Console.WriteLine("=================================");
+                foreach(var line in lines)
+                {
+
+                }
+                Console.WriteLine("=================================");
+            }
         }
     }
 }
